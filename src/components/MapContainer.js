554cb8d83e4ef29d53b204d01e4c4739a36eb8df
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-07-27 15:38:33 -0700

    Add structure for hardcoded country locations

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -12,42 +12,27 @@ let googleMapURL =
   APIKEY +
   "&v=3.exp&libraries=geometry,drawing,places";
 
-var geocode = function(country) {
-  //API call to geocode url
-  var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-      var anHttpRequest = new XMLHttpRequest();
-      anHttpRequest.onreadystatechange = function() {
-        if (anHttpRequest.readyState === 4 && anHttpRequest.status === 200)
-          aCallback(anHttpRequest.responseText);
-      };
-
-      anHttpRequest.open("GET", aUrl, true);
-      anHttpRequest.send(null);
-    };
-  };
-  var client = new HttpClient();
-  client.get(
-    "https://maps.googleapis.com/maps/api/geocode/json?address=" +
-      country +
-      "&key=" +
-      APIKEY,
-    function(response) {
-      let result = {
-        lat: JSON.parse(response).results[0].geometry.location.lat,
-        lng: JSON.parse(response).results[0].geometry.location.lng
-      };
-      return result;
-    }
-  );
-};
-
 const Map = withScriptjs(
   withGoogleMap(props => (
     <GoogleMap defaultZoom={1} defaultCenter={{ lat: 30, lng: 0 }}>
-      {props.isMarkerShown && <Marker position={props.markerPosition} />}
       <div>
-        <Marker position={{ lat: 0, lng: 0 }} />
+        {/* TBD fix slow http api request and fix hardcoded country lat-longs, got values from https://developers.google.com/maps/documentation/geocoding/intro */}
+        <Marker position={props.France} />
+        {/* USA */}
+        <Marker
+          position={{
+            lat: 37.09024,
+            lng: -95.712891
+          }}
+        />
+
+        {/* Canada */}
+        <Marker
+          position={{
+            lat: 56.130366,
+            lng: -106.346771
+          }}
+        />
       </div>
     </GoogleMap>
   ))
@@ -59,7 +44,45 @@ export default class MapContainer extends React.Component {
     this.state = {};
   }
 
+  HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+      var anHttpRequest = new XMLHttpRequest();
+      anHttpRequest.onreadystatechange = function() {
+        if (anHttpRequest.readyState === 4 && anHttpRequest.status === 200)
+          aCallback(anHttpRequest.responseText);
+      };
+      anHttpRequest.open("GET", aUrl, true);
+      anHttpRequest.send(null);
+    };
+  };
+
+  geocode = countryArray => {
+    var that = this;
+    var client = new this.HttpClient();
+    countryArray.forEach(element => {
+      client.get(
+        "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+          element +
+          "&key=" +
+          APIKEY,
+        function(response) {
+          let result = {
+            lat: JSON.parse(response).results[0].geometry.location.lat,
+            lng: JSON.parse(response).results[0].geometry.location.lng
+          };
+          var key = element;
+          var obj = {};
+          obj[key] = result;
+          that.setState(obj);
+        }
+      );
+    });
+  };
+
   render() {
+    //TBD fix slow-loading issue with adding more array elements, Map props, and Marker props
+    this.geocode(["France"]);
+
     return (
       <div style={{ height: "100%" }}>
         <Map
@@ -68,6 +91,7 @@ export default class MapContainer extends React.Component {
           containerElement={<div style={{ height: `55vh` }} />}
           mapElement={<div style={{ height: `100%` }} />}
           placeMarker={this.placeMarker}
+          France={this.state.France}
         />
       </div>
     );
